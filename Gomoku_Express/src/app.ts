5c9f1abecf08ef39c4b3a4468587b3aa09cc9a91
Mark <markgatus@hotmail.com>
2022-09-14 07:39:58 +1000

    Setup Mongoose DB, need to add DbURI to env

--- Gomoku_Express/src/app.ts
+++ Gomoku_Express/src/app.ts
@@ -1,8 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import connectDB from './util/connectDB';
 import gamesHandler from './handler/games.handler';
 
 dotenv.config();
+
+//connect to database
+connectDB();
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -14,6 +20,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World')
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+// only listen to request when DB connection is established
+mongoose.connection.once('connected', () => {
+    console.log('⚡️[server]: Connected to MongoDB.');
+    app.listen(port, () => {
+        console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    });
 })
+
+//app.listen(port, () => {
+//    console.log(`[server]: Server is running at http://localhost:${port}`);
+//})
